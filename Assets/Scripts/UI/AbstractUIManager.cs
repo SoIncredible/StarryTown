70eeafb177782d196ae261def241b98ebe433e0e
李一诺 <2636060652@qq.com>
2023-07-12 08:42:54 +0800

    [feat]: 编写了UIManager，还没写完

---
+++ Assets/Scripts/UI/AbstractUIManager.cs
@@ -0,0 +1,32 @@
+﻿using Mono.CompilerServices.SymbolWriter;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class AbstractUIManager : MonoBehaviour
+    {
+        public Canvas UICanvas { get; private set; }
+        
+        public CanvasScaler UIScaler { get; private set; }
+        
+        public RectTransform Root { get; private set; }
+
+        public EventSystem EventSystem { get; private set; }
+
+        protected void InitInternal(Canvas canvas)
+        {
+            UICanvas = canvas;
+            UIScaler = canvas.GetComponent<CanvasScaler>();
+            Root = canvas.GetComponent<RectTransform>();
+            EventSystem = EventSystem.current;
+        }
+
+        protected void ReleaseInternal()
+        {
+            
+        }
+
+    }
+}
