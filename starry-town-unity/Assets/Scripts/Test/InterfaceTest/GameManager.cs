e89f2ca6903c4cf20159684c4b1e5e8697816e7f
李一诺 <2636060652@qq.com>
2023-07-30 13:57:52 +0800

    [art]: 基本完成UI的绘制

---
+++ starry-town-unity/Assets/Scripts/Test/InterfaceTest/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace Test.InterfaceTest
+{
+    public class GameManager : MonoBehaviour
+    {
+        [Range(0f, 1f)] [SerializeField] private float a = 0.5f;
+
+
+        public float A
+        {
+            get => a;
+            set => a = value;
+        }
+
+
+        [SerializeField] private MyData _myData;
+
+
+        private void Start()
+        {
+            _myData = new MyData();
+            _myData.DataName = "TestJson";
+            _myData.DataLevel = 6;
+            _myData.DataHealth = 75.0f;
+
+
+            string jsonData = JsonUtility.ToJson(_myData);
+            Debug.Log("Serialized JSON Data" + jsonData);
+
+            MyData myDataTest = JsonUtility.FromJson<MyData>(jsonData);
+
+
+            Debug.Log("Deserialize Data Name:" + myDataTest.DataName);
+            Debug.Log("Deserialized Data Level:" + myDataTest.DataLevel);
+            Debug.Log("Deserialized Data Health:" + myDataTest.DataHealth);
+        }
+    }
+
+    [Serializable]
+    public class MyData
+    {
+        public string DataName;
+        public int DataLevel;
+        public float DataHealth;
+    }
+}
