e5beffad9742dec49d7b0a3bf4af3810299d391c
李一诺 <liyinuo@dailyinnovation.biz>
2023-08-11 15:01:36 +0800

    [feat]: 编写SettingsManager和ConfigManager

--- starry-town-unity/Assets/Scripts/Config/ConfigManager.cs
+++ starry-town-unity/Assets/Scripts/Config/ConfigManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using SingleInputSettingItemConfig = Config.SettingsConfig.SingleInputSettingItemConfig;
 
@@ -8,16 +9,63 @@ namespace Config
     {
         public static ConfigManager Instance;
 
+        private const string InputSettingsKey = "InputSettings";
+
+
         public static void Create()
         {
             if (Instance == null)
             {
                 Instance = new ConfigManager();
             }
+
+            if (PlayerPrefs.HasKey(InputSettingsKey))
+            {
+                string json = PlayerPrefs.GetString(InputSettingsKey);
+                Instance.LoadSettingsFromJson(json);
+            }
+            else
+            {
+                // 没有自定义过
+                Instance.LoadDefaultSettings();
+            }
         }
 
 
+        public List<SingleInputSettingItemConfig> GetConfigList()
+        {
+            return _list;
+        }
+
+        public void GetOneConfig()
+        {
+        }
+
+        private void LoadSettingsFromJson(string json)
+        {
+            // data
+        }
+
+        private void LoadDefaultSettings()
+        {
+        }
+
+        private string SaveSettingsToJson()
+        {
+            SettingsConfig config = new SettingsConfig();
+
+            string json = JsonUtility.ToJson(config);
+
+            PlayerPrefs.SetString(InputSettingsKey, json);
+
+            return json;
+        }
+
+        // 如果在PlayerPrefs中没有存储有用户自定义的配置 则从配置文件中加载
+
+
         // TODO: 将该数据替换成Config
+        // 暂时使用PlayerPrefs持久化存储玩家的设置
         private readonly List<SingleInputSettingItemConfig> _list =
             new List<SingleInputSettingItemConfig>()
             {
--- starry-town-unity/Assets/Scripts/Settings/InputSettingsManager.cs
+++ starry-town-unity/Assets/Scripts/Settings/InputSettingsManager.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using Config;
 using DefaultNamespace;
+using UnityEngine;
 
 namespace Settings
 {
@@ -7,12 +10,72 @@ namespace Settings
         // 输入相关设置
         public static InputSettingsManager Instance;
 
+        // const 和 readonly 的区别
+        private Dictionary<string, SettingsConfig.SingleInputSettingItemConfig> _settingDic;
+
+        private const string InputSettingsKey = "InputSettings";
+
         public static void Creat()
         {
             if (Instance == null)
             {
                 Instance = new InputSettingsManager();
             }
+
+            Instance.LoadDefaultSettings();
+        }
+
+
+        public void ResetInputSettings()
+        {
+            // 重制所有的设置
+        }
+
+
+        private void LoadSettingsFromJson(string json)
+        {
+            // data
+        }
+
+        private void LoadSettingsFromConfig()
+        {
+        }
+
+        private void LoadDefaultSettings()
+        {
+            var list = ConfigManager.Instance.GetConfigList();
+
+            foreach (var item in list)
+            {
+                _settingDic.Add(item.ActionText, item);
+            }
+        }
+
+        private void LoadPlayerPref()
+        {
+            foreach (var item in _settingDic.Keys)
+            {
+                if (PlayerPrefs.HasKey(item))
+                {
+                    // PlayerPrefs中保存了玩家自定义的数据，如果在Prefs中找不到key，说明这是新加入游戏的配置，可以从Config中加载
+                    string json = PlayerPrefs.GetString(InputSettingsKey);
+                    Instance.LoadSettingsFromJson(json);
+                }
+                else
+                {
+                }
+            }
+        }
+
+        private string SaveSettingsToJson()
+        {
+            SettingsConfig.SingleInputSettingItemConfig config = new SettingsConfig.SingleInputSettingItemConfig();
+
+            string json = JsonUtility.ToJson(config);
+
+            PlayerPrefs.SetString(InputSettingsKey, json);
+
+            return json;
         }
     }
 }
